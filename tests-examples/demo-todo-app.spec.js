0c19fbbab0c8c6b30841b8a5e764fe8836d208c1
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-11-09 10:10:05 +0530

    Update demo-todo-app.spec.js

--- tests-examples/demo-todo-app.spec.js
+++ tests-examples/demo-todo-app.spec.js
@@ -3,6 +3,7 @@ const { test, expect } = require('@playwright/test');
 test.beforeEach(async ({ page }) => {
   
   await page.goto('https://demo.playwright.dev/todomvc');
+  
 });
 const TODO_ITEMS = [
   'buy some cheese',
