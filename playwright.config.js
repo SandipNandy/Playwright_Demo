29dfa45ee161d0b4e69c09bdfb26972de5509b8e
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-12-19 08:59:31 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -42,7 +42,6 @@ module.exports = defineConfig({
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
