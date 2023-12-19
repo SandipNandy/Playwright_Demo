ec20048f81ca081e85e2810d9d276e0783de4f58
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-12-19 08:59:17 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -34,8 +34,6 @@ module.exports = defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-
-      
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
